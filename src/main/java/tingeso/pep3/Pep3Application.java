b40a4b6262ea14c7e1c710a35c3b89130a085c51
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-07-12 14:22:49 -0400

    Initial commit

---
+++ src/main/java/tingeso/pep3/Pep3Application.java
@@ -0,0 +1,13 @@
+package tingeso.pep3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tingeso/pep3/Pep3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.pep3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
